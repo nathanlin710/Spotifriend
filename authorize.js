96f5a910717c466b0f5079e03cecd17284bdf2f0
Eric Yang <ericyang@erics-mbp.lan>
2020-10-03 00:39:35 -0700

    starting authorization

--- authorize.js
+++ authorize.js
@@ -1,8 +1,6 @@
-const Http = new XMLHttpRequest();
-const url='https://accounts.spotify.com/authorize?client_id=6484b8d0c92b492dbb82ee07222e4002&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09';
-Http.open("GET", url);
-Http.send();
-
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
+function authorizeUser() {
+  const Http = new XMLHttpRequest();
+  const url= 'https://accounts.spotify.com/authorize?client_id=6484b8d0c92b492dbb82ee07222e4002&response_type=code&redirect_uri=http://localhost:8888/callback&scope=user-read-private%20user-read-email&state=34fFs29kd09';
+  Http.open("POST", url);
+  Http.send();
 }
